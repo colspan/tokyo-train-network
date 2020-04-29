9b2d5953d946f55e377f79ff1a44a720a0ee3e11
Kunihiko MIYOSHI <miyoshik@gmail.com>
2020-04-29 17:26:27 +0900

    add colormap for line

--- src/app.js
+++ src/app.js
@@ -4,6 +4,39 @@ import { StaticMap } from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
 import { GeoJsonLayer } from '@deck.gl/layers';
 import { scaleLinear, scaleThreshold } from 'd3-scale';
+import colormap from 'colormap';
+
+const edgeColor = colormap({
+  colormap: 'inferno',
+  nshades: 256,
+  format: 'rba',
+  alpha: 1
+}).map(d => [d[0], d[1], d[2], 255]);
+const edgeScale = scaleLinear()
+  .clamp(true)
+  .domain([0, 15000])
+  .range([0, 255]);
+const CAPACITY_TIME_RANGES = [
+  'capacity_0200_0659',
+  'capacity_0700_0729',
+  'capacity_0730_0759',
+  'capacity_0800_0829',
+  'capacity_0830_0859',
+  'capacity_0900_0929',
+  'capacity_0930_0959',
+  'capacity_1000_1059',
+  'capacity_1100_1259',
+  'capacity_1300_1459',
+  'capacity_1500_1659',
+  'capacity_1700_1759',
+  'capacity_1800_1859',
+  'capacity_1900_1959',
+  'capacity_2000_2059',
+  'capacity_2100_2159',
+  'capacity_2200_2259',
+  'capacity_2300_2359',
+  'capacity_2400_0159',
+];
 
 
 export default class App extends Component {
@@ -11,19 +44,37 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      hoveredObject: null,
+      timeIndex: 5,
     };
     this._onHover = this._onHover.bind(this);
     this._renderTooltip = this._renderTooltip.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log(nextProps);
     // if (nextProps.accidents !== this.props.accidents) {
     //   this.setState({
     //     ...this._aggregateAccidents(nextProps.accidents)
     //   });
     // }
+  }
+
+  _getLineColor(f, timeIndex) {
+    const value = Math.abs(f.properties[`${CAPACITY_TIME_RANGES[timeIndex]}_up`] - f.properties[`${CAPACITY_TIME_RANGES[timeIndex]}_down`]);
+    if (value) {
+      return edgeColor[Math.floor(edgeScale(value))];
+    }
+    else {
+      return [0, 0, 0, 50];
+    }
+  }
+  _getLineWidth(f, timeIndex) {
+    const value = Math.abs(f.properties[`${CAPACITY_TIME_RANGES[timeIndex]}_up`] - f.properties[`${CAPACITY_TIME_RANGES[timeIndex]}_down`]);
+    if (value) {
+      return edgeScale(value) / 255 * 30 + 10;
+    }
+    else {
+      return 1;
+    }
   }
 
   _onHover({ x, y, object }) {
@@ -39,14 +90,15 @@ export default class App extends Component {
         id: 'geojson',
         data: trainNetwork,
         stroked: true,
-        filled: false,
-        lineWidthMinPixels: 0.5,
+        filled: true,
+        lineWidthMinPixels: 5,
+        lineJointRounded: true,
         parameters: {
           depthTest: false
         },
 
-        // getLineColor: f => this._getLineColor(f, fatalities[year]),
-        // getLineWidth: f => this._getLineWidth(f, incidents[year]),
+        getLineColor: d => this._getLineColor(d, this.state.timeIndex),
+        getLineWidth: d => this._getLineWidth(d, this.state.timeIndex),
 
         pickable: true,
         onHover: this._onHover,
@@ -102,22 +154,23 @@ export default class App extends Component {
     const mapStyle = {
       version: 8,
       sources: {
-        'osm-tiles': {
+        'cyberjapandata-tiles': {
           type: 'raster',
           tiles: [
-            'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            'http://b.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            // 'https://cyberjapandata.gsi.go.jp/xyz/english/{z}/{x}/{y}.png',
+            'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg',
           ],
-          tileSize: 256
+          tileSize: 256,
+          'attribution': '',
         }
       },
       layers: [
         {
           id: 'simple-tiles',
           type: 'raster',
-          source: 'osm-tiles',
-          minzoom: 0,
-          maxzoom: 22
+          source: 'cyberjapandata-tiles',
+          minzoom: 5,
+          maxzoom: 22,
         }
       ]
     };
